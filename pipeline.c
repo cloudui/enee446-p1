2ea15b66071681610d5032d655e20110afcabb8b
Eric Chen <ericchen314@gmail.com>
2024-02-20 01:49:18 -0500

    Initial commit

---
+++ pipeline.c
@@ -0,0 +1,33 @@
+
+/*
+ * 
+ * pipeline.c
+ * 
+   This is the primary place for student's code.
+
+ */
+
+
+#include <stdlib.h>
+#include <string.h>
+#include "fu.h"
+#include "pipeline.h"
+
+void
+writeback(state_t *state, int *num_insn) {
+}
+
+
+void
+execute(state_t *state) {
+}
+
+
+int
+decode(state_t *state) {
+}
+
+
+void
+fetch(state_t *state) {
+}
